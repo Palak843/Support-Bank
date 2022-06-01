adfb6fb38019c93419de179f3781ce42fb7e17e2
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-06-01 15:25:55 +0100

    adding transactions class

--- Support-Bank/Program.cs
+++ Support-Bank/Program.cs
@@ -4,8 +4,15 @@ using System;
         public class Program {
             public static void Main (String[] args)
             {
+                // TotalMoney totalMoney = new TotalMoney();
+                // totalMoney.CreateUserTotalMoney();
+
                 Transactions transactions = new Transactions();
-                transactions.CreateUserTotalMoney();
+                List<string[]> userTransaction = transactions.ReturnListOfTransactions("Jon A");
+                foreach(var item in userTransaction)
+                {
+                    Console.WriteLine($"Date : {item[0]}, From : {item[1]}, To : {item[2]}, Description : {item[3]}, Amount : {item[4]}");
+                }
 
             }
         }
--- Support-Bank/Transactions.cs
+++ Support-Bank/Transactions.cs
@@ -2,10 +2,16 @@ namespace SupportBank
 {
     class Transactions
     {
-        public Dictionary <string,decimal> UserTotalMoney {get; set;} = new Dictionary<string, decimal>();
-        
-        public Dictionary <string,decimal> CreateUserTotalMoney()
+
+        public string[]CreateTransactions(string date, string from, string to, string narrative, string amount)
         {
+            string[] transaction = {date, from, to, narrative, amount};
+            return transaction;
+        }
+
+        public List<string[]> ReturnListOfTransactions(string User)
+        {
+            List<string[]> AllUserTransactions = new List<string[]>();
             using (var reader = new StreamReader("Transactions2014.csv"))
             {
                 while (!reader.EndOfStream)
@@ -13,33 +19,14 @@ namespace SupportBank
                     var line = reader.ReadLine();
                     if (line == null) continue;
                     var values = line.Split(',');
-                    if (!UserTotalMoney.ContainsKey(values[1]))
+                    if (values[1] == User || values[2] == User)
                     {
-                        UserTotalMoney.Add(values[1], decimal.Parse(values[4]) * -1);
-                        Console.WriteLine(values[1]);
+                        string[] listElement = CreateTransactions(values[0], values[1], values[2], values[3], values[4]);
+                        AllUserTransactions.Add(listElement);
                     }
-                    else
-                    {
-                        UserTotalMoney[values[1]] -= decimal.Parse(values[4]);
-                    }
-                    if (!UserTotalMoney.ContainsKey(values[2]))
-                    {
-                        UserTotalMoney.Add(values[2], decimal.Parse(values[4]));
-                        Console.WriteLine(values[2]);
-                    }
-                    else
-                    {
-                        UserTotalMoney[values[2]] += decimal.Parse(values[4]);
-                    }
-                    }
+                }
             }
-            foreach(KeyValuePair<string, decimal> entry in UserTotalMoney)
-            {
-                Console.WriteLine(entry);
-            }
-
-            return UserTotalMoney;
-            }
+            return AllUserTransactions;
         }
-        
     }
+}
